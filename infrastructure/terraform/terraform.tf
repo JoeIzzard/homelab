e615b88c0dc543b6d2bbf6d0aa299e38c882d673
JoeIzzard <Joe@Izzard.me>
2025-02-24 00:22:51 +0000

    feat: add proxmox provider

--- infrastructure/terraform/terraform.tf
+++ infrastructure/terraform/terraform.tf
@@ -9,6 +9,10 @@ terraform {
       source = "1Password/onepassword"
       version = "2.1.2"
     }
+    proxmox = {
+      source = "bpg/proxmox"
+      version = "0.70.1"
+    }
   }
 
   backend "s3" {
@@ -31,4 +35,30 @@ terraform {
 // -------------------------------------------------------------------------------------------------
 provider "onepassword" {
   // No need to authenticate, we use the OP_SERVICE_ACCOUNT_TOKEN environment variable
+}
+
+// -------------------------------------------------------------------------------------------------
+// Proxmox
+// -------------------------------------------------------------------------------------------------
+
+data "onepassword_item" "pve_credentials" {
+  vault = data.onepassword_vault.homelab.uuid
+  title = "Proxmox Terraform"
+}
+
+locals {
+  pve_endpoint = data.onepassword_item.pve_credentials.hostname
+  pve_insecure = data.onepassword_item.pve_credentials.section[index(data.onepassword_item.pve_credentials.section.*.label, "connection")].field[index(data.onepassword_item.pve_credentials.section[index(data.onepassword_item.pve_credentials.section.*.label, "connection")].field.*.label, "insecure")].value
+  pve_token = format("%s=%s", data.onepassword_item.pve_credentials.username, data.onepassword_item.pve_credentials.credential)
+  pve_ssh_username = data.onepassword_item.pve_credentials.section[index(data.onepassword_item.pve_credentials.section.*.label, "connection")].field[index(data.onepassword_item.pve_credentials.section[index(data.onepassword_item.pve_credentials.section.*.label, "connection")].field.*.label, "ssh_username")].value
+}
+
+provider "proxmox" {
+    endpoint = local.pve_endpoint
+    insecure = local.pve_insecure
+    api_token = local.pve_token
+    ssh {
+      username = local.pve_ssh_username
+      password = data.onepassword_item.pve_credentials.password
+    }
 }
